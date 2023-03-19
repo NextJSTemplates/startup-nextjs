c1db6f0502d6720ce55f1b580aa26581114096e8
Muhammad Rizky Anditama <rizkind@gmail.com>
2023-03-19 17:03:44 +0700

    update landing page

--- app/providers.jsx
+++ app/providers.jsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }) {
   return (
-    <ThemeProvider attribute="class" enableSystem="false" defaultTheme="dark">
+    <ThemeProvider attribute="class" enableSystem="false" defaultTheme="light">
       {children}
     </ThemeProvider>
   );

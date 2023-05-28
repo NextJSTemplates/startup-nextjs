68bcdd304f9f1c67374a4e2867ccf15ea483f7c0
Musharof Chowdhury <mosarrof121@gmail.com>
2023-05-28 14:58:34 +0600

    Update index.tsx

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -22,10 +22,10 @@ const Hero = () => {
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
-                    href="https://nextjstemplates.com/templates/startup"
+                    href="https://nextjstemplates.com/templates/saas-starter-startup"
                     className="rounded-md bg-primary py-4 px-8 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
-                    Download Now
+                   🔥 Get Pro
                   </Link>
                   <Link
                     href="https://github.com/NextJSTemplates/startup-nextjs"

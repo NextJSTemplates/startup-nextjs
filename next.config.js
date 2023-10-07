4885790038d98e461abdb63de8060de4f5631024
Hamza Yousaf <hamzay050@gmail.com>
2023-10-08 02:30:16 +0500

    es linter disabled

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;

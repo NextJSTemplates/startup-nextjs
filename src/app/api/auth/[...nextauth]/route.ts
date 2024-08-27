437e942589edbf9a888d5e46736d50f572d5d8c7
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-08-27 19:12:27 +0600

    Adding strategy and providers

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth/next";
 
 const handler = NextAuth({
+  session: {
+    strategy: "jwt",
+  },
+  providers: [],
+});
 
-})
-
-export {handler as GET, handler as POST}
+export { handler as GET, handler as POST };

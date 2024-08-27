e9bddb7cf6931de108b2d92d6146dc0538b357e3
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-08-27 23:12:23 +0600

    Added bcrypt for password hashing

--- src/app/signup/api/route.ts
+++ src/app/signup/api/route.ts
@@ -1,4 +1,5 @@
 import { connectDB } from "@/lib/connectDB";
+import bcrypt from "bcrypt";
 
 export const POST = async (request) => {
   const newUser = await request.json();
@@ -10,7 +11,11 @@ export const POST = async (request) => {
     if (exist) {
       return Response.json({ message: "User Exists" }, { status: 304 });
     }
-    const resp = await userCollection.insertOne(newUser);
+    const hashedPassword = bcrypt.hashSync(newUser.password, 14);
+    const resp = await userCollection.insertOne({
+      ...newUser,
+      password: hashedPassword,
+    });
     return Response.json({ message: "User created" }, { status: 200 });
   } catch (error) {
     return Response.json(

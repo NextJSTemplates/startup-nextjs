96a93c347f4b46b13d34f764176919af1f8c4626
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-09-16 10:08:17 +0000

    Added signup form on home page

--- src/components/About/AboutSectionOne.tsx
+++ src/components/About/AboutSectionOne.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
 import SectionTitle from "../Common/SectionTitle";
+import SocialButton from "../SocialSignIn";
+import FormComponent from "../Form";
+import Link from "next/link";
 
 const checkIcon = (
   <svg width="16" height="13" viewBox="0 0 16 13" className="fill-current">
@@ -21,20 +24,21 @@ const AboutSectionOne = () => {
     <section id="about" className="pt-16 md:pt-20 lg:pt-28">
       <div className="container">
         <div className="border-b border-body-color/[.15] pb-16 dark:border-white/[.15] md:pb-20 lg:pb-28">
-          <div className="-mx-4 flex flex-wrap items-center">
-            <div className="w-full px-4 lg:w-1/2">
+          <div className="flex gap-10 flex-col justify-center">
+            <div className="w-full px-4">
               <SectionTitle
+                center
                 title="Sign up today for latest updates"
                 paragraph="Sign up for get latest update and start boost up your business from today."
                 mb="44px"
               />
 
               <div
-                className="mb-12 max-w-[570px] lg:mb-0"
+                className="mb-12 lg:mb-0"
                 data-wow-delay=".15s"
               >
-                <div className="mx-[-12px] flex flex-wrap">
-                  <div className="w-full px-3 sm:w-1/2 lg:w-full xl:w-1/2">
+                <div className="flex">
+                  <div className="w-full justif-center flex flex-col items-end px-3 sm:w-1/2 lg:w-full xl:w-1/2">
                     <List text="fast service" />
                     <List text="Reliable Price" />
                     <List text="Quality work" />
@@ -49,21 +53,22 @@ const AboutSectionOne = () => {
               </div>
             </div>
 
-            <div className="w-full px-4 lg:w-1/2">
-              <div className="relative mx-auto aspect-[25/24] max-w-[500px] lg:mr-0">
-                <Image
-                  src="/images/about/about-image.svg"
-                  alt="about-image"
-                  fill
-                  className="mx-auto max-w-full drop-shadow-three dark:hidden dark:drop-shadow-none lg:mr-0"
-                />
-                <Image
-                  src="/images/about/about-image-dark.svg"
-                  alt="about-image"
-                  fill
-                  className="mx-auto hidden max-w-full drop-shadow-three dark:block dark:drop-shadow-none lg:mr-0"
-                />
+            <div className="rounded bg-white w-full md:w-1/2 mx-auto shadow-three p-4 dark:bg-dark">
+              <SocialButton></SocialButton>
+              <div className="mb-8 flex items-center justify-center">
+                <span className="hidden h-[1px] w-full max-w-[60px] bg-body-color/50 sm:block"></span>
+                <p className="w-full px-5 text-center text-base font-medium text-body-color">
+                  Or, register with your email
+                </p>
+                <span className="hidden h-[1px] w-full max-w-[60px] bg-body-color/50 sm:block"></span>
               </div>
+              <FormComponent></FormComponent>
+              <p className="text-center text-base font-medium text-body-color">
+                Already using Startup?{" "}
+                <Link href="/signin" className="text-primary hover:underline">
+                  Sign in
+                </Link>
+              </p>
             </div>
           </div>
         </div>

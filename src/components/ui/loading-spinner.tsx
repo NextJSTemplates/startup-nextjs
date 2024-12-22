0dee4b769456411e40feb3183103635c07ee7886
Rosario <marcodibartolo2@gmail.com>
2024-12-22 15:11:27 +0100

    dding react icons and loading spinner

---
+++ src/components/ui/loading-spinner.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { TbLoaderQuarter } from "react-icons/tb";
+
+function LoadingSpinner() {
+  return (
+    <TbLoaderQuarter className = " animate-spin " size = {30} />
+  )
+}
+
+export default LoadingSpinner

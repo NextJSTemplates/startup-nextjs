63168891ebebd553bd7ca6654f385a65f846e42f
johurul-haque <johurulhaquejony@gmail.com>
2025-04-10 10:50:09 +0600

    resolve build errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
 import { Inter } from "next/font/google";
-import "node_modules/react-modal-video/css/modal-video.css";
 import "../styles/index.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -35,3 +34,4 @@ export default function RootLayout({
 }
 
 import { Providers } from "./providers";
+

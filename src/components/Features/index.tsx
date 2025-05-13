52bedce12a6c8155c3baac6c45c7ccd7789c7edd
Cynthia Umwali <cynthiaumwali3@gmail.com>
2025-05-13 23:15:02 +0200

    feat: add technology stacks to Features component

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -113,16 +113,36 @@ const Features = () => {
       title: "DevOps",
       text: "Get access to custom software for your unique needs including frontend and backend technology.",
       icon: "/images/features/code.svg",
+      stack: [
+        { title: "Ansible", icon: "/images/features/devops/ansible.svg" },
+        { title: "Chef", icon: "/images/features/devops/chef.svg" },
+        { title: "Docker", icon: "/images/features/devops/ansible.svg" },
+        { title: "Gitlab", icon: "/images/features/devops/gitlab.svg" },
+      ],
     },
     {
       title: "Back-End",
       text: "Develop user-friendly experiences with server-oriented applications that cater to changing market needs and robust user convenience.",
       icon: "/images/features/android.svg",
+      stack: [
+        { title: "Rails", icon: "/images/features/bcend/rails.svg" },
+        { title: "Ruby", icon: "/images/features/bcend/ruby.svg" },
+        { title: "Strapi", icon: "/images/features/bcend/strapi.svg" },
+        { title: "Gitlab", icon: "/images/features/bcend/gitlab.svg" },
+      ],
     },
     {
       title: "Infrastructure",
       text: "Solutions that help organizations scale their resources as per changing demands. We enable the best pathways in the cloud journey.",
       icon: "/images/features/cloud.svg",
+      stack: [
+        { title: "AWS", icon: "/images/features/infrastructure/aws.svg" },
+        {
+          title: "Digital Ocean",
+          icon: "/images/features/infrastructure/docean.svg",
+        },
+        { title: "Heroku", icon: "/images/features/infrastructure/heroku.svg" },
+      ],
     },
   ];
 
@@ -131,16 +151,35 @@ const Features = () => {
       title: "Front-End",
       text: "Create top-rated UI solutions that offer personalized user experience with quality generated content.",
       icon: "/images/features/searchstar.svg",
+      stack: [
+        { title: "CSS 3", icon: "/images/features/frtend/css.svg" },
+        { title: "HTML 5", icon: "/images/features/frtend/html.svg" },
+        { title: "JS", icon: "/images/features/frtend/js.svg" },
+        { title: "React", icon: "/images/features/frtend/react.svg" },
+        { title: "Redux", icon: "/images/features/frtend/redux.svg" },
+        { title: "Vue", icon: "/images/features/frtend/vue.svg" },
+      ],
     },
     {
       title: "Databases",
       text: "Through Machine Learning Algorithms and AI, we help businesses leverage technologies to drive automation and improve experiences.",
       icon: "/images/features/chip.svg",
+            stack: [
+        { title: "Postgres", icon: "/images/features/dtbase/postgres.svg" },
+
+      ],
+
     },
     {
       title: "Integrations",
       text: "Immersive solutions behind the physical and virtual world offering customized experiences across various industries.",
       icon: "/images/features/vr.svg",
+            stack: [
+        { title: "Rails", icon: "/images/features/bcend/rails.svg" },
+        { title: "Ruby", icon: "/images/features/bcend/ruby.svg" },
+        { title: "Strapi", icon: "/images/features/bcend/strapi.svg" },
+        { title: "Gitlab", icon: "/images/features/bcend/gitlab.svg" },
+      ],
     },
   ];
 
@@ -188,7 +227,7 @@ const Features = () => {
                 />
               );
             } else {
-              // Dashed vertical 
+              // Dashed vertical
               return (
                 <line
                   key={`vert-${idx}`}

e9a47b1c3b30ddb63fc5f393346b03f8a037e3fe
Cynthia Umwali <cynthiaumwali3@gmail.com>
2025-05-13 18:41:04 +0200

    fix: include className prop in SectionTitle

--- src/components/Common/SectionTitle.tsx
+++ src/components/Common/SectionTitle.tsx
@@ -1,15 +1,17 @@
 const SectionTitle = ({
   title,
   width = "w-[30%]",
+  className
 }: {
   title: string;
   width?: string;
+  className?: string
 }) => {
   return (
       <div
-        className={`mb-12 rounded-se-[70px] bg-black py-6 text-center text-white shadow-md ${width}`}
+        className={`mb-12 rounded-se-[70px] bg-black py-6 text-center text-white shadow-md ${width} ${className}`}
       >
-        <h2 className="text-lg font-bold leading-tight! sm:text-xl md:text-2xl">
+        <h2 className="text-[14px] font-bold leading-tight! sm:text-xl md:text-2xl">
           {title}
         </h2>
       </div>

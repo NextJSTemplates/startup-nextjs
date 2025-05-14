e162737586a8ad2dc8fbccee6c193e699e8fdb3a
Cynthia Umwali <cynthiaumwali3@gmail.com>
2025-05-14 12:22:56 +0200

    feat: update Hero component layout and enhance styling for team members

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -31,8 +31,8 @@ const Hero = () => {
         className="relative z-10 overflow-hidden bg-[url('/images/hero/background.svg')] bg-cover bg-center pt-[120px] pb-8 md:pt-[160px] md:pb-[16px] xl:pt-[210px] xl:pb-[90px] 2xl:pt-[180px] 2xl:pb-[80px]"
       >
         <div className="relative z-10 container mx-auto px-4">
-          <div className="flex flex-wrap items-center gap-12 lg:gap-0">
-            <div className="w-full md:w-2/3">
+          <div className="grid grid-cols-1 md:grid-cols-3 items-center gap-12 lg:gap-0">
+            <div className="w-full md:col-span-2">
               <div className="max-w-[800px] text-start text-white">
                 <h5 className="mb-6 text-sm tracking-widest text-white uppercase">
                   Empower. Innovate. Transform
@@ -62,7 +62,7 @@ const Hero = () => {
                   </Link>
                   <Link
                     href="#"
-                    className="flex gap-2 rounded-full border border-white px-6 py-3 text-sm font-bold text-white hover:bg-white hover:text-black"
+                    className="flex gap-2 rounded-full border border-white px-4 py-3 text-sm font-bold text-white hover:bg-white hover:text-black"
                   >
                     OUR SERVICES
                     <ArrowRight className="h-4 w-4" />
@@ -71,13 +71,13 @@ const Hero = () => {
               </div>
             </div>
 
-            <div className="relative mt-12 w-full md:mt-0 md:w-1/3">
+            <div className="relative mt-12 w-full md:mt-0">
               <div className="grid grid-cols-2 gap-4">
                 <div className="flex -translate-y-6 flex-col items-center space-y-6">
                   {teamOne.map((member, index) => (
                     <div
                       key={index}
-                      className={`transform ${index % 2 === 0 ? "translate-x-2" : "-translate-x-6"} overflow-hidden rounded-xl border border-white bg-transparent p-4`}
+                      className={`transform ${index % 2 === 0 ? "translate-x-3 lg:translate-x-2" : "-translate-x-2 lg:-translate-x-6"} overflow-hidden rounded-xl border border-white bg-transparent p-4 backdrop-blur-[4px] shadow-2xl`}
                     >
                       <div>
                         <img
@@ -96,7 +96,7 @@ const Hero = () => {
                   {teamTwo.map((member, index) => (
                     <div
                       key={index}
-                      className={`transform ${index % 2 === 0 ? "translate-x-2" : "-translate-x-6"} overflow-hidden rounded-xl border border-white bg-transparent p-4`}
+                      className={`transform ${index % 2 === 0 ? "translate-x-8 lg:translate-x-2" : "translate-x-4 lg:-translate-x-6"} overflow-hidden rounded-xl border border-white bg-transparent p-4 backdrop-blur-[4px] shadow-2xl`}
                     >
                       <div>
                         <img

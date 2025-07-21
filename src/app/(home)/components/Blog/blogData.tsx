5a5d2dfbde7348fa238ca58ec8cd93e835f67567
Cynthia Umwali <cynthiaumwali3@gmail.com>
2025-07-21 16:14:52 +0200

    Update blogs

--- src/app/(home)/components/Blog/blogData.tsx
+++ src/app/(home)/components/Blog/blogData.tsx
@@ -7,18 +7,18 @@ const blogPosts = [
     description: "Explore project lifecycle, from design to development"
   },
   {
-    title: "The Importance of Design: A Comprehensive Exploration",
-    date: "Jan 14, 2025",
-    image: "/images/blog/blog2.webp",
-    href: "https://blog.classyendeavors.com/the-importance-of-design-a-comprehensive-exploration-23a822024bdf",
-    description: "What is Design and Why Does It Matter?"
+    title: "Enhance Your Design Workflow with Styles and Plugins",
+    date: "Apr 1, 2025",
+    image: "/images/blog/blog-2.webp",
+    href: "https://blog.classyendeavors.com/enhance-your-design-workflow-with-styles-and-plugins-cb6c6eb49b09",
+    description: "Whether you’re new to design or a seasoned pro, Figma is a fantastic tool that can make your work easier and more efficient"
   },
   {
-    title: "Context Caching vs RAG: A Comprehensive Guide to Enhancing LLM Performance",
-    date: "Jan 4, 2025",
-    image: "/images/blog/blog3.webp",
-    href: "https://blog.classyendeavors.com/context-caching-vs-rag-a-comprehensive-guide-to-enhancing-llm-performance-dbf7470c99b1",
-    description: "Understanding Context Caching and Retrieval-Augmented Generation (RAG)"
+    title: "Understanding Higher-Order Components in React: A Beginner’s Guide",
+    date: "Sept 26, 2024",
+    image: "/images/blog/blog-3.webp",
+    href: "https://blog.classyendeavors.com/understanding-higher-order-components-in-react-a-beginners-guide-85e0b2a9015b",
+    description: "React is a powerful library for building user interfaces, and one of its most useful patterns is the Higher-Order Component (HOC)"
   }
 ];
 

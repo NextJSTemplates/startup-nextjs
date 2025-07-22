f93d75cf9a3b8fa6015ec7dc3eecf7699d83de1b
Cynthia Umwali <cynthiaumwali3@gmail.com>
2025-07-22 17:56:06 +0200

    update About Us Hero with typewriter effect

--- src/app/about-us/components/Hero.tsx
+++ src/app/about-us/components/Hero.tsx
@@ -1,40 +1,31 @@
-import { Cpu, ShieldCheck, Layers, Zap } from "lucide-react";
 import { Scene } from "@/components/hero-section";
 import { Button } from "@/components/ui/moving-border";
 import Link from "next/link";
+import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
+import { useEffect, useState } from "react";
+const words1 = [
 
-const features = [
   {
-    icon: Cpu,
-    title: "Performance",
-    description: "Ultra-fast data processing in every situation.",
-  },
+    text: "Innovation"
+  }
+]
+const words2 = [
   {
-    icon: ShieldCheck,
-    title: "Security",
-    description: "Advanced protection for complete peace of mind.",
-  },
-  {
-    icon: Layers,
-    title: "Modularity",
-    description: "Easy integration with existing architecture.",
-  },
-  {
-    icon: Zap,
-    title: "Responsiveness",
-    description: "Instant response to every command.",
-  },
-
-];
-
+    text: "Integrity",
+    className: "text-primary"
+  }
+]
 const Hero = () => {
   return (
     <div className="min-h-svh bg-linear-to-br from-[#000] to-[#1A2428] text-white flex flex-col items-center justify-center p-8">
       <div className="w-full max-w-6xl space-y-12 relative z-10">
         <div className="flex flex-col items-center text-center space-y-8">
           <div className="space-y-6 flex items-center justify-center flex-col ">
-            <h1 className="text-3xl md:text-6xl font-semibold tracking-tight max-w-3xl">
-              Driven by Innovation. Defined by Integrity
+            <h1 className="text-base sm:text-xl md:text-3xl lg:text-6xl font-bold max-w-3xl flex gap-2">
+              Driven by {" "} <TypewriterEffectSmooth words={words1} />
+            </h1>
+            <h1 className="text-base sm:text-xl md:text-3xl lg:text-6xl font-bold max-w-3xl flex gap-2">
+              Defined by {" "} {<TypewriterEffectSmooth words={words2} />}
             </h1>
             <p className="text-sm md:text-base text-neutral-300 max-w-2xl">
               At Classy Endeavors, we&apos;re more than a software development agency — we&apos;re your strategic technology partner. With a passionate team of designers, developers, and engineers, we craft scalable, user-focused solutions that align with your business goals. From idea to execution, we commit to transparency, quality, and long-term impact
--- src/app/about-us/components/Innovations.tsx
+++ src/app/about-us/components/Innovations.tsx
@@ -33,8 +33,8 @@ const Innovations = () => {
               src="/images/about/Vector.png"
               alt="Speaker"
               className="w-full lg:scale-[1.1] hover:scale-[1.15] transition duration-300"
-              width={400}  // Increased from 100
-              height={400} // Increased from 100
+              width={400} 
+              height={400} 
               quality={100}
               priority
             />
